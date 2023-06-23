62d18a597d97f2b062797c49bcbf7b779e1aa36e
Angie <caritocon@gmail.com>
2023-06-23 13:46:49 -0700

    Wave 4 completed

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -3,45 +3,59 @@ import PropTypes from 'prop-types';
 import { useState} from 'react';
 
 const INITIAL_FORM_DATA = {
-    title: "",
+    title: '',
     id:0,   
-}
+};
 
 const NewTaskForm = (props) => {
     const [taskFormData, setTaskFormData] = useState(INITIAL_FORM_DATA);
-   
+
     const anInputChanged = (event) => {
         console.log(event);
 
         if (event.target.title === 'id' && event.target.value) {
             return
         }
-            
+        
+        const newTaskFormData = {
+            ...taskFormData,
+            [event.target.name]: event.target.value
+        };
+        setTaskFormData(newTaskFormData);
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        props.createNewTask(taskFormData);
+        setTaskFormData(INITIAL_FORM_DATA);
     };
 
 
     return (
         <div>
-            <form>
+            <form className='stack' onSubmit={handleFormSubmit}>
                 <label htmlFor="taskTitle">Title:</label>
                 <input 
-                id="taskId" 
-                title="title"
-                /> 
-                
-
+                    id="taskId" 
+                    name='title'
+                    type="text"
+                    value={ taskFormData.title}
+                    onChange={ anInputChanged} />
+                <label htmlFor="taskTitle">id:</label>
+                <input 
+                    id="taskId" 
+                    name='id'
+                    type="number"
+                    value={ taskFormData.id}
+                    onChange={ anInputChanged} />
+                <input type='submit' value='Add new Task' />
             </form>
-            
         </div>
     )
 };
 
 NewTaskForm.Proptypes = {
     createNewTask: PropTypes.func
-}
-
-
-
-
+};
 
 export default NewTaskForm;
